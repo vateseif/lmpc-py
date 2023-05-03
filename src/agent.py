f1bfbfff1ae4a3c7dd2f4587c415fd1b14d3bd5c
Seif <vateseif@gmail.com>
2023-05-03 22:18:48 +0200

    Add example of simple_v2 environment from pettingzoo

---
+++ src/agent.py
@@ -0,0 +1,48 @@
+import os
+import sys
+sys.path.append(os.path.abspath(''))
+
+import lmpc
+import numpy as np
+
+def getAgent():
+  N = 1
+  Ns = 4
+  Na = 4
+  T = 20
+  dt = 0.1        # TODO get val from env
+  tau = 0.25      # TODO get val from env
+  sensitivity = 5 # TODO get val from env
+  eps = 1e-3
+  Q = np.zeros((Ns,Ns))
+  R = np.eye(Na)
+
+  A = np.array([[0, 0, 1, 0], 
+                [0, 0, 0, 1], 
+                [0, 0, -tau/dt, 0], 
+                [0, 0, 0, -tau/dt]])
+  B = np.array([[0, 0, 0, 0], 
+                [0, 0, 0, 0], 
+                [1, -1, 0, 0], 
+                [0, 0, 1, -1]]) 
+
+  Ad = np.eye(Ns) + A*dt
+  Bd = B*dt*sensitivity
+
+  sys = lmpc.DistributedLTI(N, Ns, Na)
+  sys.loadAB(Ad, Bd)
+
+  controller = lmpc.LMPC(T)
+  controller << sys
+  # objective
+  controller.addObjectiveFun(lmpc.objectives.QuadForm(Q, R))
+  # box constraints control inputs
+  controller.addConstraint(lmpc.BoundConstraint('u', 'upper', (1-eps)*np.ones((sys.Nu,1))))
+  controller.addConstraint(lmpc.BoundConstraint('u', 'lower', eps * np.ones((sys.Nu,1))))
+
+  xT = np.zeros((2,1))
+  G = np.concatenate((np.eye(2), np.zeros((2,2))), axis=1)
+  controller.addConstraint(lmpc.TerminalConstraint(xT, G))
+  controller._setupSolver(np.zeros((4,1)))
+  return controller
+
---
+++ src/simple_v2.py
@@ -0,0 +1,41 @@
+import os
+import sys
+import numpy as np
+sys.path.append(os.path.abspath(''))
+
+from agent import getAgent
+from pettingzoo.mpe import simple_v2
+
+"""
+Simulation in the simple_v2 environment from pettingzo
+"""
+
+# init env
+env = simple_v2.env(max_cycles=100, continuous_actions=True, render_mode="human")
+env.reset()
+
+# init agent
+agent = getAgent()
+# initial condition
+p = np.expand_dims(env.env.world.policy_agents[0].state.p_pos.copy(), -1)
+dp = np.expand_dims(env.env.world.policy_agents[0].state.p_vel.copy(), -1)
+agent.model.setInitialState(np.concatenate((p, dp)))
+
+for i, ag in enumerate(env.agent_iter()):
+    observation, reward, termination, truncation, info = env.last()
+    # update terminal constraint
+    xT = np.array([[observation[2]], [observation[3]]]) + agent.model._x[:2]
+    agent.constraints[-1].xTd.value = xT  
+    
+    if termination or truncation:
+        break
+    else:
+        #action = env.action_space(ag).sample()
+        u, _, Phi = agent.solve(agent.model._x, solver="MOSEK")
+        agent.model.updateState(agent.model.step(u))
+        action = np.concatenate(([0], u.squeeze()), dtype=np.float32)
+    
+    env.step(action)
+
+
+env.close()
