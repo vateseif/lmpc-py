a4c8ee0c96898d60df10c6cf699eeb375a885160
vateseif <vateseif@gmail.com>
2023-05-05 19:18:10 +0200

    Created TerminalQuadForm for terminal state cost function

--- src/lmpc/objectives.py
+++ src/lmpc/objectives.py
@@ -7,6 +7,9 @@ from src.lmpc.core import ObjectiveFunc
 
 class LMPCObjectiveFun(ObjectiveFunc):
 
+  def _initFromParent(self, parent):
+    vars(self).update(vars(parent))
+
   def compute(self, 
               T: int,
               x0: np.ndarray,
@@ -35,7 +38,7 @@ class QuadForm(LMPCObjectiveFun):
       self.xud = np.zeros((phi.shape[0], 1))
 
     # compute system response
-    Nx = self.Q.shape[0]
+    Nx = self.Nx
     xu = phi[:, :Nx] @ x0[:Nx]  # (Nx*(T+1)+Nu*T, 1)
     
     QT = np.kron(np.eye(T+1), self.Q)
@@ -43,3 +46,26 @@ class QuadForm(LMPCObjectiveFun):
     QR = block_diag(QT, RT)
     
     return cp.quad_form(xu-self.xud, QR)
+
+class TerminalQuadForm(LMPCObjectiveFun):
+  
+  def __init__(self, Q:np.ndarray, xTd: np.ndarray, G:Optional[np.ndarray]=None) -> None:
+    ''' (G @ xT - xTd).T @ Q @ (G @ xT - xTd) '''
+    self.Q = Q
+    self.G = G      # basically the selction matrix
+    self.xTd = xTd
+
+  def compute(self, T: int, x0: np.ndarray, phi: cp.Variable):
+    Nx = self.Nx
+    if self.G is None:
+      self.G = np.eye(Nx)
+
+    assert self.G.shape[0] == self.xTd.shape[0], "dim 0 of G doesnt match xTd"
+    assert self.G.shape[1] == Nx
+    assert self.G.shape[1] == Nx, "dim 1 of G is not Nx"
+    assert self.Q.shape[0] == self.G.shape[0]
+
+    self.xTd = cp.Parameter(self.xTd.shape, value=self.xTd)
+    xT = phi[Nx*T:Nx*(T+1), :Nx] @ x0[:Nx]
+
+    return cp.QuadForm(self.G @ xT - self.xTd, self.Q)
