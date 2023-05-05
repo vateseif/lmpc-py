0db76af06a161b2f47ab05991a9301af103abdb1
vateseif <vateseif@gmail.com>
2023-05-05 19:18:53 +0200

    MPC adversary follows agent example

--- src/mpe/simple_tag/main.py
+++ src/mpe/simple_tag/main.py
@@ -1,21 +1,24 @@
 import numpy as np
+from mpe.agent import Agent
 from pettingzoo.mpe import simple_tag_v2
 
 
-env = simple_tag_v2.env(num_adversaries=1, num_obstacles=0, max_cycles=25, continuous_actions=True, render_mode='human')
-
+env = simple_tag_v2.env(num_adversaries=1, num_obstacles=0, max_cycles=100, continuous_actions=True, render_mode='human')
 env.reset()
-for i, agent in enumerate(env.agent_iter()):
+
+agent = Agent(1, "tag")
+
+for i, agent_name in enumerate(env.agent_iter()):
     observation, reward, termination, truncation, info = env.last()
-    print(agent, i)
-    print(env.env.world.agents[int(i%2)].state.p_pos)
-    print(env.env.world.agents[int(i%2)].state.p_vel)
-    print(observation)
+
     if termination or truncation:
         break
     else:
-        action = env.action_space(agent).sample()
-    env.step(np.ones((5,), dtype=np.float32) * 1e-3)
+      if agent_name == "agent_0":
+        action = env.action_space(agent_name).sample()
+      else:
+        action = agent.act(observation)
+    
+    env.step(action)
 
-    print("----------------------------------------------")
 env.close()
