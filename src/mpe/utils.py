7e1e6c28924bc81d846bc54eadaa02c0073ded7b
vateseif <vateseif@gmail.com>
2023-05-09 00:33:28 +0200

    save model with highest avg_reward

--- src/mpe/utils.py
+++ src/mpe/utils.py
@@ -1,5 +1,7 @@
 import torch
 import numpy as np
+from datetime import datetime
+
 
 @torch.no_grad()
 def evaluate_policy(agent, adversary, env, eval_episodes = 10):
@@ -19,4 +21,25 @@ def evaluate_policy(agent, adversary, env, eval_episodes = 10):
   agent.policy.train()
   avg_reward /= eval_episodes
   return avg_reward
+
+class SaveBestModel:
+  """
+  Class to save the best model while training. If the current epoch's 
+  average reward is more than the previous highest, then save the
+  model state.
+  """
+  def __init__(self, best_avg_reward=-float('inf')):
+    self.best_avg_reward = best_avg_reward
+      
+  def __call__(self, current_avg_reward:float, epoch:int, model:torch.nn.Module, optimizer):
+    if current_avg_reward > self.best_avg_reward:
+      self.best_avg_reward = current_avg_reward
+      print(f"\nBest validation loss: {self.best_avg_reward}")
+      print(f"\nSaving best model for epoch: {epoch+1}\n")
+      torch.save({
+          'epoch': epoch+1,
+          'model_state_dict': model.state_dict(),
+          'optimizer_state_dict': optimizer.state_dict(),
+          #'loss': criterion,
+          }, f'models/tag/best_{datetime.now()}.pth')
   
