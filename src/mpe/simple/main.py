3f212801f9dffe6b1c4aa48f7869256f6158ff88
vateseif <vateseif@gmail.com>
2023-05-26 17:26:28 +0200

    Change to local reference frame of agent

--- src/mpe/simple/main.py
+++ src/mpe/simple/main.py
@@ -16,21 +16,21 @@ agent = getAgent()
 # initial condition
 p = np.expand_dims(env.env.world.policy_agents[0].state.p_pos.copy(), -1)
 dp = np.expand_dims(env.env.world.policy_agents[0].state.p_vel.copy(), -1)
-agent.model.setInitialState(np.concatenate((p, dp)))
 
 for i, ag in enumerate(env.agent_iter()):
     observation, reward, termination, truncation, info = env.last()
     # update terminal constraint
-    xT = np.array([[observation[2]], [observation[3]]]) + agent.model._x[:2]
+    xT = np.array([[observation[2]], [observation[3]]])
     agent.constraints[-1].xTd.value = xT  
-    
+    # initial state
+    x0 = np.concatenate((np.ones((2,1))*1e-3, np.array([[observation[0]], [observation[1]]])))
+
     if termination or truncation:
         break
     else:
         #action = env.action_space(ag).sample()
-        u, _, Phi = agent.solve(agent.model._x, solver="SCS")
-        agent.model.updateState(agent.model.step(u))
-        action = np.concatenate(([0], u.squeeze()), dtype=np.float32)
+        u, _, Phi = agent.solve(x0, solver="SCS")
+        action = np.concatenate(([1e-3], u.squeeze()), dtype=np.float32)
     
     env.step(action)
 
