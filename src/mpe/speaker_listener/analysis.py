cafb3a64c67419f0f55cd6de23bfc2aa4264c721
vateseif <vateseif@gmail.com>
2023-06-08 00:12:49 +0200

    Analyze gumbel and vq over range of colors

---
+++ src/mpe/speaker_listener/analysis.py
@@ -0,0 +1,15 @@
+from itertools import product
+
+from Trainer import Trainer
+
+min_landmarks = 3
+max_landmarks = 26 # len of alphabet
+landmarks_range = list(range(min_landmarks, max_landmarks+1))
+
+s_types = ["VQ", "Gumbel"] # types of speaker to test
+
+if __name__ == "__main__":
+  for n, s in product(landmarks_range, s_types):
+    trainer = Trainer(num_landmarks=n, s_type=s, save_results=True)
+    trainer.train()
+    trainer.evaluate()
