cdc5f0552d907af415323fac3cf6db7fb091a6f1
vateseif <vateseif@gmail.com>
2023-08-12 19:24:54 +0200

    Rename gpt.py to llm.py

--- src/l2o/llm.py
+++ src/l2o/llm.py
@@ -7,7 +7,6 @@ from config.config import OptimizationDesignerConfig, TaskPlannerConfig
 
 from typing import List
 from pydantic import BaseModel, Field
-from prompts.stack import TASK_PLANNER_PROMPT, OPTIMIZATION_DESIGNER_PROMPT 
 from langchain.schema import (
     AIMessage,
     HumanMessage,
@@ -30,42 +29,50 @@ class Plan(BaseModel):
   tasks: List[str] = Field(description="list of all tasks that the robot has to carry out")
   
 class Optimization(BaseModel):
-  reward: str = Field(description="reward function to be applied to MPC written in cvxpy")
-  constraints: List[str] = Field(description="constraints to tbe applied to MPC written in cvxpy")
-
+  objective: str = Field(description="objective function to be applied to MPC")
+  #constraints: List[str] = Field(description="constraints to tbe applied to MPC written in cvxpy")
 
 
 class TaskPlanner(AbstractLLM):
-  def __init__(self, cfg: TaskPlannerConfig()) -> None:
+  def __init__(self, cfg=TaskPlannerConfig()) -> None:
     super().__init__(cfg)
-    # init GPT model
-    self.model = ChatOpenAI(model_name=self.cfg.model_name, temperature=self.cfg.temperature)
     # init parser
     self.parser = PydanticOutputParser(pydantic_object=Plan)
-
-    system_message_prompt = SystemMessagePromptTemplate.from_template(template)
+    # init GPT model
+    self.model = ChatOpenAI(model_name=self.cfg.model_name, temperature=self.cfg.temperature)
+    # init system prompt
+    system_message_prompt = SystemMessagePromptTemplate.from_template(self.cfg.prompt)
     chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt])
-
     self.prompt = chat_prompt.format_prompt(format_instructions=self.parser.get_format_instructions()).to_messages()
 
-
   def run(self) -> Plan:
-    plan = self.parser.parse(self.model(self.prompt))
+    output = self.model(self.prompt)
+    plan = self.parser.parse(output.content)
+    print(plan)
     return plan 
 
 
 class OptimizationDesigner(AbstractLLM):
   def __init__(self, cfg=OptimizationDesignerConfig()) -> None:
     super().__init__(cfg)
-    self.messages = [SystemMessage(content=self.cfg.prompt)]
+    # init parser
+    self.parser = PydanticOutputParser(pydantic_object=Optimization)
+    # init prompt
+    system_prompt = SystemMessagePromptTemplate.from_template(self.cfg.prompt)
+    self.messages = [system_prompt.format(format_instructions=self.parser.get_format_instructions())]    
+    # init model
     self.model = ChatOpenAI(model_name=self.cfg.model_name, temperature=self.cfg.temperature)
+
 
   def run(self, user_message:str) -> str:
     self.messages.append(HumanMessage(content=user_message))
     model_message = self.model(self.messages)
     self.messages.append(model_message)
     print(f"\033[91m {model_message.content} \033[0m")
-    return model_message.content
+    return self.parser.parse(model_message.content)
 
 
 
+OptimizationDesignerOptions = {
+  "objective": OptimizationDesigner
+}
