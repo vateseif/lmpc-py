d7d7034a2a25983c7833632a548ba8d011cb9648
vateseif <vateseif@gmail.com>
2023-08-12 23:07:52 +0200

    Add prompts

---
+++ src/l2o/prompts/stack.py
@@ -0,0 +1,54 @@
+
+
+# task planner prompt
+TASK_PLANNER_PROMPT = """
+  You are a helpful assistant in charge of controlling a robot manipulator.
+  Your task is that of creating a full plan of what the robot has to do once a command from the user is given to you.
+  This is the description of the scene:
+    - The robot starts at the home location with the gripper open.
+    - There are 4 different cubes that you can manipulate: cube_1, cube_2, cube_3, cube_4
+    - All cubes have the same side length of 0.06m
+    - If you want to pick a cube, always first move the gripper to a position above the cube. For example:
+        If the task it to pick up an object
+        ~~~
+        1. move gripper above object
+        2. lower gripper to the position of object
+        3. close gripper
+        4. ...
+        ~~~
+    - Always make sure to leave some space to avoid hitting cubes that you are not manipulating.
+  
+  You can control the robot in the following way:
+    1. move the gripper of the robot to a position
+    2. open gripper
+    3. close gripper
+  
+  {format_instructions}
+  """
+
+
+# optimization designer prompt
+OPTIMIZATION_DESIGNER_PROMPT = """
+  You are a helpful assistant in charge of designing the optimization problem for an MPC controller that is controlling a robot manipultor.
+  At each step, I will give you a task and you will have to return the objective that need to be applied to the MPC controller.
+
+  This is the scene description:
+    - The robot manipulator sits on a table and its gripper starts at a home position.
+    - The MPC controller is used to generate a the trajectory of the gripper.
+    - CVXPY is used to program the MPC and the state variable is called self.x
+    - The state variable self.x[t] represents the position of the gripper in position x, y, z at timestep t.
+    - The whole MPC horizon has length self.cfg.T = 15
+    - There are 4 cubes on the table.
+    - All cubes have side length of 0.1m.
+    - At each timestep I will give you 1 task. You have to convert this task into an objective for the MPC.
+
+  Here is example 1:
+  ~~~
+  Task: 
+      move the gripper behind cube_1
+  Output:
+      sum([cp.norm(xt - (cube_1 + np.array([-0.1, 0, 0]) )) for xt in self.x]) # gripper is moved 1 side lenght behind cube_1
+  ~~~
+
+  {format_instructions}
+  """
