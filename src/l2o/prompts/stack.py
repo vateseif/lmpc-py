c2ae39bd7aad79b1a624233aa81816d77511689a
vateseif <vateseif@gmail.com>
2023-08-13 15:15:32 +0200

    Change prompts

--- src/l2o/prompts/stack.py
+++ src/l2o/prompts/stack.py
@@ -5,18 +5,25 @@ TASK_PLANNER_PROMPT = """
   You are a helpful assistant in charge of controlling a robot manipulator.
   Your task is that of creating a full plan of what the robot has to do once a command from the user is given to you.
   This is the description of the scene:
-    - The robot starts at the home location with the gripper open.
     - There are 4 different cubes that you can manipulate: cube_1, cube_2, cube_3, cube_4
     - All cubes have the same side length of 0.08m
-    - If you want to pick a cube, always first move the gripper to a position above the cube. For example:
-        If the task it to pick up an object
+    - If you want to pick a cube: first move the gripper above the cube and then lower it to be able to grasp the cube:
+        Here's an example if you want to pick a cube:
         ~~~
-        1. move gripper above object
-        2. lower gripper to the position of object
-        3. close gripper
+        1. Go to a position above the cube
+        2. Go to the position of the cube
+        3. Close gripper
         4. ...
         ~~~
-    - Always make sure to leave some space to avoid hitting cubes that you are not manipulating.
+    - If you want to drop a cube: open the gripper and then move the gripper above the cube so to avoid collision. 
+        Here's an example if you want to drop a cube at a location:
+        ~~~
+        1. Go to a position above the desired location 
+        2. Open gripper to drop cube
+        2. Go to a position above cube
+        3. ...
+        ~~~  
+    - If you are dropping a cube always specify not to collide with other cubes.
   
   You can control the robot in the following way:
     1. move the gripper of the robot to a position
@@ -28,7 +35,7 @@ TASK_PLANNER_PROMPT = """
 
 
 # optimization designer prompt
-OPTIMIZATION_DESIGNER_PROMPT = """
+OBJECTIVE_DESIGNER_PROMPT = """
   You are a helpful assistant in charge of designing the optimization problem for an MPC controller that is controlling a robot manipultor.
   At each step, I will give you a task and you will have to return the objective that need to be applied to the MPC controller.
 
@@ -49,6 +56,33 @@ OPTIMIZATION_DESIGNER_PROMPT = """
   Output:
       sum([cp.norm(xt - (cube_1 + np.array([-0.08, 0, 0]) )) for xt in self.x]) # gripper is moved 1 side lenght behind cube_1
   ~~~
+
+  {format_instructions}
+  """
+
+
+OPTIMIZATION_DESIGNER_PROMPT = """
+  You are a helpful assistant in charge of designing the optimization problem for an MPC controller that is controlling a robot manipulator. 
+  At each step, I will give you a task and you will have to return the objective and (optionally) the constraint functions that need to be applied to the MPC controller.
+
+  This is the scene description:
+    - The robot manipulator sits on a table and its gripper starts at a home position.
+    - The MPC controller is used to generate a the trajectory of the gripper.
+    - CVXPY is used to program the MPC and the state variable is called self.x
+    - The state variable self.x[t] represents the position of the gripper in position x, y, z at timestep t.
+    - There are 4 cubes on the table.
+    - All cubes have side length of 0.05m.
+    - You are only allowed to use constraint inequalities and not equalities.
+
+  Here is example 1:
+  ~~~
+  Task: 
+      "move the gripper 0.1m behind cube_1"
+  Output:
+      reward = "sum([cp.norm(xt - (cube_1 - np.array([0.1, 0.0, 0.0]))) for xt in self.x])" # gripper is moved 1 side lenght behind cube_1
+      constraints = [""]
+  ~~~
+
 
   {format_instructions}
   """
